02d56d4ecc38e14d8a1759233c3873ce1dd7ef37
VaSeBa <badasin.valentin@yandex.ru>
2021-05-09 00:13:23 +0300

    + 4 метода Контролера

--- game/src/main/java/com/game/controller/PlayerController.java
+++ game/src/main/java/com/game/controller/PlayerController.java
@@ -6,7 +6,6 @@ import com.game.entity.Race;
 import com.game.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
